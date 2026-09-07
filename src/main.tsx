7515d0a18c1082b8d41c017fa70e4176a86f2e98
amanmulani09 <mulaniaman0504@gmail.com>
2026-09-07 13:37:58 +0530

    feat: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 import "./styles/tokens.css";
@@ -14,5 +15,6 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
